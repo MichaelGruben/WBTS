c9a41378921ccb2926286ba8472172e78eeb931e
Benjamin Merkle <merkle-benjamin@web.de>
2013-04-18 16:49:22 +0200

    Topic + Test

--- MasterlyMate/app/models/topic.rb
+++ MasterlyMate/app/models/topic.rb
@@ -1,3 +1,48 @@
 class Topic < ActiveRecord::Base
-  attr_accessible :name
+  attr_accessible :name, :childTopics
+  
+  def initialize(name)
+    @name = name
+    childTopics = Array.new(0)
+  end
+ #Methods 
+  def addTopic(topic)
+    childTopics[childTopics.length] = topic
+  end
+  
+  def searchTopicByName(nameOfSearchedTopic)
+    i = 0
+    while i < childTopics.length do
+      if childTopics[i].name == nameOfSearchedTopic do
+        return topic
+      end
+      i += 1
+    end
+    return -1
+  end
+  
+  def deleteTopic(nameDeleteTopic)
+    i = 0
+    while i < childTopics.length do
+      if childTopics[i].name == nameDeleteTopic do
+        j = 0
+        while j < i do
+          tempArray1[j] = childTopics[j]
+          j += 1
+        end
+        
+        j = childTopics.length - 1
+        while j > i do
+          tempArray2[j] = childTopics[j]
+          j -= 1
+        end
+        childTopics = tempArray1 + tempArray2
+      end
+    end
+  end
+  
+  def getAllTopics()
+    return childTopics
+  end
+  
 end
